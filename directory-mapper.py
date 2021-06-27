c6669992f5cfbbdbf1e8556a8021963ec3778aac
dlschoof <dlschoof@mtu.edu>
2021-06-27 18:55:23 -0400

    Added Directory & File printing

--- directory-mapper.py
+++ directory-mapper.py
@@ -2,18 +2,37 @@ import os
 import sys
 
 
-def main(path):
+def main(path, depth):
     cdcontents = []
     cddirectories = []
 
     for item in os.listdir(path):
-        if os.path.isfile(path+'/'+item):
+        if os.path.isfile(path+'/'+item) and item[0] != '.':
             cdcontents.append(item)
-        else:
+        elif item[0] != '.':
             cddirectories.append(item)
 
-    print(cddirectories)
-    print(cdcontents)
+    print_files(cdcontents, depth)
+    print_dir(path, cddirectories, depth)
+
+
+def print_files(file_list, depth):
+    for file in file_list:
+        substr = ''
+        for _ in range(depth):
+            substr += '-'
+
+        print(substr+file)
+
+
+def print_dir(path, dir_list, depth):
+    for dir in dir_list:
+        substr = ''
+        for _ in range(depth):
+            substr += '-'
+
+        print(substr+dir)
+        main(path+'/'+dir, depth+1)
 
 
 if __name__ == '__main__':
@@ -24,5 +43,6 @@ if __name__ == '__main__':
         if not os.path.exists(path):
             print('[-] Invalid path given.')
             sys.exit()
-
-    main(path)
+    print()
+    main(path, 0)
+    print()
